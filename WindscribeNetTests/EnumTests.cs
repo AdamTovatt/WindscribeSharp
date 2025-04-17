73a163210437486eb4414aff0961ebd6f105f6b3
Adam Tovatt <sporedenstore@hotmail.com>
2025-04-17 23:41:00 +0200

    add more enum tests

--- WindscribeNetTests/EnumTests.cs
+++ WindscribeNetTests/EnumTests.cs
@@ -19,5 +19,30 @@ namespace WindscribeTests
             Assert.AreEqual(ActiveState.Off, EnumConverter.FromString<ActiveState>("off"));
             Assert.AreEqual(ActiveState.Off, EnumConverter.FromString<ActiveState>("OFF"));
         }
+
+        [TestMethod]
+        public void ConvertInternetConnectivity()
+        {
+            Assert.AreEqual("available", EnumConverter.ToString(InternetConnectivity.Available));
+            Assert.AreEqual("unavailable", EnumConverter.ToString(InternetConnectivity.Unavailable));
+
+            Assert.AreEqual(InternetConnectivity.Available, EnumConverter.FromString<InternetConnectivity>("available"));
+            Assert.AreEqual(InternetConnectivity.Available, EnumConverter.FromString<InternetConnectivity>("AVAILABLE"));
+
+            Assert.AreEqual(InternetConnectivity.Unavailable, EnumConverter.FromString<InternetConnectivity>("unavailable"));
+            Assert.AreEqual(InternetConnectivity.Unavailable, EnumConverter.FromString<InternetConnectivity>("UNAVAILABLE"));
+        }
+
+        [TestMethod]
+        public void ConvertFirewallState()
+        {
+            Assert.AreEqual("Off", EnumConverter.ToString(FirewallState.Off));
+            Assert.AreEqual("On", EnumConverter.ToString(FirewallState.On));
+            Assert.AreEqual("Always On", EnumConverter.ToString(FirewallState.AlwaysOn));
+
+            Assert.AreEqual(FirewallState.Off, EnumConverter.FromString<FirewallState>("off"));
+            Assert.AreEqual(FirewallState.On, EnumConverter.FromString<FirewallState>("on"));
+            Assert.AreEqual(FirewallState.AlwaysOn, EnumConverter.FromString<FirewallState>("always on"));
+        }
     }
 }
